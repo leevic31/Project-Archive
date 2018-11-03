f9dc9fa899a8abfb9677ad54cb861b80dbf9b665
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-11-03 16:06:19 -0400

    split ui into teq and agency

--- src/agency_ui.py
+++ src/agency_ui.py
@@ -11,6 +11,8 @@ from PyQt5.QtWidgets import (
     QMessageBox,
     QPushButton,
     QTabWidget,
+    QTableWidget,
+    QTableWidgetItem,
     QVBoxLayout,
     QWidget
     )
@@ -34,71 +36,19 @@ class agencyWidget(QWidget):
 
         # Initialize tab screen
         self.tab_widget = QTabWidget()
-        self.tabs = [iCareNewTemplateWidget(), iCareUploadWidget()]
+#        self.tabs = [iCareNewTemplateWidget(), iCareUploadWidget()]
 
-        self.tab_widget.addTab(self.tabs[0], "Add New Template")
-        self.tab_widget.addTab(self.tabs[1], "Upload iCare Data")
+#        self.tab_widget.addTab(self.tabs[0], "Add New Template")
+#        self.tab_widget.addTab(self.tabs[1], "Upload iCare Data")
+
+        self.tabs = [iCareUploadWidget()]
+
+        self.tab_names = ["Upload iCare Data"]
+        for i in range(len(self.tabs)):
+            self.tab_widget.addTab(self.tabs[i], self.tab_names[i])
 
         self.layout.addWidget(self.tab_widget)
         self.setLayout(self.layout)
-
-
-class iCareNewTemplateWidget(QWidget):
-    def __init__(self):
-        super(QWidget, self).__init__()
-        self.filepaths = []
-        self._setup_widget()
-
-    def _setup_widget(self):
-        # set layouts
-        self.layout = QGridLayout(self)
-        self.layout.setColumnStretch(0, 1)
-        self.layout.setColumnStretch(1, 3)
-
-        # widgets
-        self.label1 = QLabel("New Template Name:")
-        self.iCare_template_name = QLineEdit(self)
-        self.file_upload_label = QLabel("No File Chosen")
-
-        self.upload1 = QPushButton("Select Template")
-        self.upload1.clicked.connect(self.file_select)
-        self.submit1 = QPushButton("Submit New Template")
-        self.submit1.clicked.connect(self.submit_new_iCare_template)
-
-        # add widgets
-        self.layout.addWidget(self.label1, 0, 0)
-        self.layout.addWidget(self.iCare_template_name, 0, 1)
-        self.layout.addWidget(self.upload1, 1, 0)
-        self.layout.addWidget(self.file_upload_label)
-        self.layout.addWidget(self.submit1, 2, 0)
-        self.setLayout(self.layout)
-
-    @pyqtSlot()
-    def file_select(self):
-        dialog = QFileDialog()
-        dialog.setFileMode(QFileDialog.AnyFile)
-        dialog.setNameFilter("xlsx (*.xlsx)")
-
-        if dialog.exec_():
-            self.filepaths = dialog.selectedFiles()
-            self.file_upload_label.setText(get_file_name(self.filepaths[0]))
-
-    @pyqtSlot()
-    def submit_new_iCare_template(self):
-        template_name = self.iCare_template_name.text()
-        if (not template_name):
-            prompt_error("Please give a Template name")
-            return
-        if (not self.filepaths):
-            prompt_error("Please select a Template file")
-            return
-
-        pyxl.add_new_template(template_name, self.filepaths[0])
-        prompt_information("New template: '{}' has been added"
-                            .format(template_name))
-
-        self.iCare_template_name.setText("")
-        self.filepaths = []
 
 class iCareUploadWidget(QWidget):
     def __init__(self):
