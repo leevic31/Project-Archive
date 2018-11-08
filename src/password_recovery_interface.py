517437792702a0a4761e03644e4e1b68664af1fa
leevic31 <leevj.lee@mail.utoronto.ca>
2018-11-08 02:26:38 -0500

    created a test for password recovery interface

--- src/password_recovery_interface.py
+++ src/password_recovery_interface.py
@@ -38,8 +38,3 @@ class Password_Recovery(QMainWindow):
         QMessageBox.question(self, 'Message', "Check " +  textboxValue + " for your login information", QMessageBox.Ok, QMessageBox.Ok)
         self.textbox.setText("")
         sendEmail(self, textboxValue)
- 
-#if __name__ == '__main__':
-    #app = QApplication(sys.argv)
-    #ex = Password_Recovery()
-    #sys.exit(app.exec_())
