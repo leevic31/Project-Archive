94fe32487921d092db905b6fea2e98ec2c43d337
Kelvin Tsang <kelvintsangwk@gmail.com>
2018-11-09 05:45:40 -0500

    Modified class name

--- src/exportFile.py
+++ src/exportFile.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 def exportCSV(save_path, file_name, query_result):
-    """Export the query  result into CSV file
+    """Export the query result into CSV file
 
     Arguments:
         save_path {String} -- the path where CSV file is saved
@@ -18,11 +18,9 @@ def exportCSV(save_path, file_name, query_result):
 
     # setup file location
     location = path.join(save_path, file_name)
-    # save query result into dataframe
-    df = pd.DataFrame(query_result)
     try:
         # export query to csv file
-        df.to_csv(location, index=False)
+        query_result.to_csv(location, index=False)
         # return true if CSV file is successfully saved
         return True
     except:
@@ -34,5 +32,5 @@ if __name__ == "__main__":
     connection = database.get_db_connection("root", "12345678", "world")
     # query.printDB(query.get_DBinfo())
     results = query.manual_sql_query(
-        connection, "select * from city")
+        connection, "select * from country")
     exportCSV("E:\project", "test.csv", results)
