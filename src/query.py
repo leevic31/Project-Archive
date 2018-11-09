7bfc18c3930912d09a3330338fd11473ac629c29
Kelvin Tsang <kelvintsangwk@gmail.com>
2018-11-09 03:24:27 -0500

    Updated query functions

--- src/query.py
+++ src/query.py
@@ -3,49 +3,53 @@ import database
 
 
 class query():
-    db = object()
 
-    def ConnectDB(connection):
-        query.db = connection
-
-    def get_DBinfo():
+    def get_DBinfo(connection):
         """obtain all field names from each table in the database
+
+        Arguments:
+            connection  -- database connection        
 
         Returns:
             defaultdict -- table name as key, a list of all field names in table as value
         """
+        try:
+            info = defaultdict(list)
+            cursor = connection.cursor()
+            # SQL command to list all table
+            cursor.execute("SHOW TABLES")
+            results = cursor.fetchall()
+            # iterate all tables
+            for result in results:
+                for key, value in result.items():
+                    # SQL command to show all column names
+                    cursor.execute("SHOW COLUMNS FROM " + value)
+                    columns = cursor.fetchall()
+                    for column in columns:
+                        # store column names in dictionary with table name as the key
+                        info[value].append(column.get("Field"))
+            return info
+        finally:
+            connection.close()
 
-        info = defaultdict(list)
-        cursor = query.db.cursor()
-        # SQL command to list all table
-        cursor.execute("SHOW TABLES")
-        results = cursor.fetchall()
-        # iterate all tables
-        for result in results:
-            for key, value in result.items():
-                # SQL command to show all column names
-                cursor.execute("SHOW COLUMNS FROM " + value)
-                columns = cursor.fetchall()
-                for column in columns:
-                    # store column names in dictionary with table name as the key
-                    info[value].append(column.get("Field"))
-        return info
-
-    def manual_sql_query(command):
+    def manual_sql_query(connection, command):
         """Run MySQL query and return the result
 
         Arguments:
+            connection  -- database connection
             command {String} -- SQL command
 
         Returns:
             Dictionary in List -- the list contains all records from query in dictionary format, while column as key and data as vaule in the dictionary 
         """
-
-        cursor = query.db.cursor()
-        # execute query
-        cursor.execute(command)
-        data = cursor.fetchall()
-        return data
+        try:
+            cursor = connection.cursor()
+            # execute query
+            cursor.execute(command)
+            data = cursor.fetchall()
+            return data
+        finally:
+            connection.close()
 
     def printDB(info):
         for key, values in info.items():
@@ -61,9 +65,7 @@ class query():
 
 
 if __name__ == "__main__":
-    query.ConnectDB(database.get_db_connection("root", "12345678", "world"))
-    # query.printDB(query.get_DBinfo())
-    selection = ["code", "name"]
-    table = "country"
+    connection = database.get_db_connection("root", "12345678", "world")
+    # query.printDB(query.get_DBinfo(connection))
     results = query.manual_sql_query(
-        "select code, name from country where continent ='Asia'")
+        connection, "select code, name from country where continent ='Asia'")
