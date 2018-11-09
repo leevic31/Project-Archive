36f9f136f9c3ee31f9e0e31e1b79aa1ae20f4364
Kelvin Tsang <kelvintsangwk@gmail.com>
2018-11-09 04:41:51 -0500

    Updated exportCSV function

--- src/exportCSV.py
+++ src/exportCSV.py
@@ -18,22 +18,15 @@ def exportCSV(save_path, file_name, query_result):
 
     # setup file location
     location = path.join(save_path, file_name)
-    # to save the column name from query result
-    column = list()
-    data = list()
-    first = False
-    if query_result:
-        for record in query_result:
-            for key in record.keys():
-                column.append(key)
-            break
-        df = pd.DataFrame(columns=column)
-        for record in query_result:
-            temp = pd.DataFrame([record], columns=record.keys())
-            df = pd.concat([df, temp], axis=0, sort=True)
+    # save query result into dataframe
+    df = pd.DataFrame(query_result)
+    try:
+        # export query to csv file
         df.to_csv(location, index=False)
+        # return true if CSV file is successfully saved
         return True
-    else:
+    except:
+        # return false if fail to save the file
         return False
 
 
@@ -41,5 +34,5 @@ if __name__ == "__main__":
     connection = database.get_db_connection("root", "12345678", "world")
     # query.printDB(query.get_DBinfo())
     results = query.manual_sql_query(
-        connection, "select * from country where continent ='Asia'")
+        connection, "select * from city")
     exportCSV("E:\project", "test.csv", results)
