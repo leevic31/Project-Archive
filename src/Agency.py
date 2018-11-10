7a2c53b6b0a9b1d70589275a500d0642bb03cb2e
DhairyaD <dhairya2136@gmail.com>
2018-11-10 18:54:56 -0500

    Added comments to Agency and rearranged a few lines of code for readability

--- src/Agency.py
+++ src/Agency.py
@@ -1,10 +1,13 @@
 import sys
-#from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QLabel
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import QSize
+import db_config
+import mysql.connector
+from mysql.connector import MySQLConnection, Error
 
-class Agency(QMainWindow):
+
+class Agency(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -17,6 +20,11 @@ class Agency(QMainWindow):
         self.agencyWindow()
 
     def agencyWindow(self):
+        # Set the title of the window as defined in __init__
+        self.setWindowTitle(self.title)
+        # Set the geometry of the window as defined in in __init__
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
         self.name = QLineEdit(self)
         self.name.setPlaceholderText("Agency Name ...")
         self.name.setGeometry(200,100,200,30)
@@ -36,11 +44,11 @@ class Agency(QMainWindow):
         self.button = QPushButton("Add", self)
         self.button.setGeometry(250,300,100,30)
 
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+
         self.show()
 
 
+# Need to create a table before inserting data into it
 if __name__ == '__main__':
     my_app = QApplication(sys.argv)
     agency = Agency()
