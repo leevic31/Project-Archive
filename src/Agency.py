88b3610eab1c7ceb2dad990dc78aa5206a7759c0
DhairyaD <dhairya2136@gmail.com>
2018-11-10 21:44:54 -0500

    implemented the add_agency method that adds agencies into a db; added more comments

--- src/Agency.py
+++ src/Agency.py
@@ -2,11 +2,10 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import QSize
-# import db_config
+import db_config
 import mysql.connector
 from mysql.connector import MySQLConnection, Error
-from dbsetup import db_connection, add_agency
-
+# from dbsetup import db_connection, add_agency
 
 class Agency(QWidget):
 
@@ -22,8 +21,7 @@ class Agency(QWidget):
         self.width = 700
         self.height = 700
         self.agencyWindow()
-        self.dbsetup.db_connection()
-        self.dbsetup.add_agency()
+
 
     def agencyWindow(self):
         # Set the title of the window as defined in __init__
@@ -54,9 +52,23 @@ class Agency(QWidget):
         # Button to add the agency to db
         self.button = QPushButton("Add", self)
         self.button.setGeometry(250,300,100,30)
+        self.button.clicked.connect(self.add_agency)
 
 
         self.show()
+
+    def add_agency(self):
+        mydb_conn = mysql.connector.connect(host=db_config.host, database=db_config.database, user=db_config.user, password=db_config.password)
+        try:
+            my_cursor = mydb_conn.cursor()
+            my_cursor.execute("USE testdb")
+            my_cursor.execute("INSERT INTO Agency(AgencyName, AgencyAddress, NumberOfEmployees, iCARETemplateChoice) VALUES('%s, %s, %s, %s')" % ( ''.join(self.name.text()) , ''.join(self.address.text()), ''.join(self.numEmployees.text()), ''.join(self.iCareTemplate.text())) )
+            QMessageBox.about(self, 'Connection', "Agency Added!")
+        except Error as error:
+            print(error)
+
+        finally:
+            mydb_conn.close()
 
 
 
