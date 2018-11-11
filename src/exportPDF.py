f632a7f0facc21c5f96c29427deba81371ac64e8
leevic31 <leevj.lee@mail.utoronto.ca>
2018-11-11 14:50:22 -0500

    added comments to export pdf function

--- src/exportPDF.py
+++ src/exportPDF.py
@@ -7,13 +7,29 @@ import pdfkit
 import os
 import query
 
+# obtain query result using manual_sql_query function
 query_result = query.manual_sql_query(connection, command)
 
 def exportToPDF(file_name, query_result):
+    """Export the query result into PDF file
+    
+    Arguments:
+        file_name {String} -- PDF file name
+        query_result {List} -- query result
+
+    Returns:
+        Boolean -- return true if PDF file is saved successfully
+        
+    """
     try:
+        # export query result to html
         query_result.to_html('result.html')
+        # convert query result from html to pdf
         pdfkit.from_file('result.html', file_name)
+        # delete the saved html
         os.remove('result.html')
+        # return true if PDF file is successfully saved
         return True
     except:
+        # return false if fail to save the file
         return False
