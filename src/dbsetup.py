42614f0c56bc55e57313fbc324094f08c7132473
DhairyaD <dhairya2136@gmail.com>
2018-11-10 19:31:19 -0500

    Finished method required to establish connection to db

--- src/dbsetup.py
+++ src/dbsetup.py
@@ -1,44 +1,24 @@
 import mysql.connector
 from mysql.connector import MySQLConnection, Error
-from configparser import ConfigParser
+import db_config
 
 
 def db_connection():
-    db_connection = db_configure('config.ini', 'mysql')
-    print(db_connection)
+    mydb = mysql.connector.connect(host=db_config.host, database=db_config.database, user=db_config.user, password=db_config.password)
+    return mydb
 
-    try:
-        print('connecting to mysql database')
-        connection = MySQLConnection(**db_connection)
+def add_agency():
+    connection = db_connection()
+    with connection:
+        try:
+            my_cursor = connection.cursor()
+            my_cursor.execute("USE testdb")
 
-        if connection.is_connected():
-            print('connection to database established.')
-        else:
-            print('connection failed.')
+        except Error as error:
+            print(error)
 
-    except Error as error:
-        print(error)
-
-    finally:
-        connection.close()
-        print('No longer connected to database.')
-
-
-def db_configure(file_name, body):
-    # parser that reads data from .ini file with initial values
-    parser = ConfigParser()
-    parser.read(file_name)
-    # Emplty dictionary to be populated with elements
-    db = {}
-
-    if parser.has_section(body):
-
-        elements = parser.items(body)
-        for i in elements:
-            db[i[0]] = i[1]
-
-    # A dictionary is returned
-    return db
+        finally:
+            connection.close()
 
 
 
