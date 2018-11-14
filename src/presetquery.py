e2990f22c4a1ede41320cbf9c0dc5b46976bbd21
Andrei <andrei.grumazescu@mail.utoronto.ca>
2018-11-14 17:40:07 -0500

    moved presetwrite api into presetquery file

--- src/presetquery.py
+++ src/presetquery.py
@@ -9,6 +9,29 @@ from pypika import MySQLQuery, Table, Field
 	# and the second column is a VARCHAR NOT NULL labeled 'querval'
 	# and the third column is a VARCHAR NOT NULL labeled 'description'
 	
+def write_preset(conn, queryin, descriptin):	
+	# to use this method you must pass in a connection,
+	# a preset query, and a description of what the query achieves
+	# it will automatically write it to the bottom of the table
+	cursor = conn.cursor()
+	quer = "ALTER TABLE Presets DROP COLUMN id;"
+	cursor.execute(quer)
+	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
+	cursor.execute(quer)
+	
+	extable = Table('Presets')
+	q = MySQLQuery.into(extable).columns("querval", "description").insert(queryin, descriptin)
+	print(q)
+	quer = str(q)
+	
+	cursor.execute(quer)
+	
+	cursor = conn.cursor()
+	quer = "ALTER TABLE Presets DROP COLUMN id;"
+	cursor.execute(quer)
+	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
+	cursor.execute(quer)
+	
 def get_preset(conn, key):
 	# to use this method you must pass in a connection
 	# and the id for the corresponding querval to return
