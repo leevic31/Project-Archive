ac5941a65bd4f3a81d2c4e4244a91f5da5ca3c05
DhairyaD <dhairya2136@gmail.com>
2018-11-20 14:31:13 -0500

    Fixed the insert query, and added employee name and password fields

--- src/Agency.py
+++ src/Agency.py
@@ -31,27 +31,37 @@ class Agency(QWidget):
 
         # Data entry for Agency Name
         self.name = QLineEdit(self)
-        self.name.setPlaceholderText("Agency Name ...")
+        self.name.setPlaceholderText("Agency Name...")
         self.name.setGeometry(200,100,200,30)
+
+        # Employee name for an agency
+        self.employeeName = QLineEdit(self)
+        self.employeeName.setPlaceholderText("Employee Name...")
+        self.employeeName.setGeometry(200,150,200,30)
+
+        # Employee password for an agency employee
+        self.employeePassword = QLineEdit(self)
+        self.employeePassword.setPlaceholderText("Password...")
+        self.employeePassword.setGeometry(200,200,200,30)
 
         # Data entry for Agency Address
         self.address = QLineEdit(self)
-        self.address.setPlaceholderText("Agency Address ...")
-        self.address.setGeometry(200,150,200,30)
+        self.address.setPlaceholderText("Agency Address...")
+        self.address.setGeometry(200,250,200,30)
 
         # Data entry for the number of employees in an agency
         self.numEmployees = QLineEdit(self)
-        self.numEmployees.setPlaceholderText("Number of employees ...")
-        self.numEmployees.setGeometry(200,200,200,30)
+        self.numEmployees.setPlaceholderText("Number of employees...")
+        self.numEmployees.setGeometry(200,300,200,30)
 
         # Data entry for the choice of the iCareTemplate used by agency
         self.iCareTemplate = QLineEdit(self)
-        self.iCareTemplate.setPlaceholderText("Choice of iCARE template ...")
-        self.iCareTemplate.setGeometry(200,250,200,30)
+        self.iCareTemplate.setPlaceholderText("Choice of iCARE template...")
+        self.iCareTemplate.setGeometry(200,350,200,30)
 
         # Button to add the agency to db
         self.button = QPushButton("Add", self)
-        self.button.setGeometry(250,300,100,30)
+        self.button.setGeometry(250,400,100,30)
         self.button.clicked.connect(self.add_agency)
 
 
@@ -59,14 +69,15 @@ class Agency(QWidget):
 
     def add_agency(self):
         mydb_conn = mysql.connector.connect(host=db_config.host, database=db_config.database, user=db_config.user, password=db_config.password)
+        mydb_conn.autocommit = True
         try:
             my_cursor = mydb_conn.cursor()
             my_cursor.execute("USE testdb")
-            my_cursor.execute("INSERT INTO Agency(AgencyName,AgencyAddress,NumberOfEmployees,iCARETemplateChoice) VALUES('%s, %s, %s, %s') % ( ''.join(self.name.text()) , ''.join(self.address.text()), ''.join(self.numEmployees.text()), ''.join(self.iCareTemplate.text())) )
-            QMessageBox.about(self, 'Connection', "Agency Added!")
+            # Add agency information to the database
+            my_cursor.execute("INSERT INTO Agency(AgencyName, AgencyAddress, NumberOfEmployees, iCARETemplateChoice, EmployeeName, EmployeePassword) VALUES('%s', '%s', '%s', '%s', '%s', '%s')" % (''.join(self.name.text()), ''.join(self.address.text()), ''.join(self.numEmployees.text()), ''.join(self.iCareTemplate.text()), ''.join(self.employeeName.text()), ''.join(self.employeePassword.text()))   )
+            # QMessageBox.about(self, 'Connection', "Agency Added!")
         except Error as error:
             print(error)
-
         finally:
             mydb_conn.close()
 
