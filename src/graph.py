d0fe5e678ef09bd799734ee37821a6a15874bef5
Kelvin Tsang <kelvintsangwk@gmail.com>
2018-11-22 05:32:13 -0500

    Removed unncessary functions in graph.py

--- src/graph.py
+++ src/graph.py
@@ -5,43 +5,6 @@ import database
 import textwrap
 
 
-def pie_chart_one_field(title, query, number):
-    """Generate a pie chart to display percentage of data in a specific field
-
-    Arguments:
-        title {String} -- the graphic title
-        query {Dataframe} -- the query result [ONE COLUMN ONLY]
-        number {int} -- the top N number of data to be displayed
-
-    Returns:
-        [plot] -- the generated plot
-    """
-
-    try:
-        # generate new dataframe to store proportion of each data in the field
-        summary = query.apply(pd.value_counts)
-        # check whether 'other' field is necessary
-        if summary.size > number:
-            # obtain the top N number of data
-            data = summary.head(number)
-            # get the sum of other proportion that is not in top N number
-            other_total = np.sum(summary.iloc[number:].values)
-            # create series and assign index 'Others' to be displayed in chart
-            df = pd.Series(data=[other_total], index=['Others'])
-            # concat the top N number of data and 'Others'
-            data = pd.concat([data[title], df])
-        else:
-            data = summary
-        # generate pie chart
-        graph = data.plot.pie(subplots=True, legend=False,
-                              autopct='%.2f', title=title)
-        plt.ylabel('')
-        return plt
-    except:
-        # return None if error
-        return None
-
-
 def pie_chart(title, query):
     """Generate pie chart to display percentage of data
 
