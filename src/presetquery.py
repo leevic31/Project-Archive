6508e10dfe391cf2ffda8a6a546dba0716d8c70c
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-11-24 17:47:32 -0500

    fix presetquery edit sql injection

--- src/presetquery.py
+++ src/presetquery.py
@@ -1,126 +1,66 @@
 import pyxl
 import mysql.connector
 
+import database
+
 from pypika import MySQLQuery, Table, Field
 
-	# the use of this function assumes there exists some Table
-	# called 'Presets' where the first column is an
-	# UNSIGNED AUTO_INCREMENT PRIMARY KEY labeled 'id'
-	# and the second column is a VARCHAR NOT NULL labeled 'querval'
-	# and the third column is a VARCHAR NOT NULL labeled 'description'
+def write_preset(query_text, query_desc):
+    # to use this method you must pass in a connection,
+    # a preset query, and a description of what the query achieves
+    # it will automatically write it to the bottom of the table
 
-def write_preset(conn, queryin, descriptin):
-	# to use this method you must pass in a connection,
-	# a preset query, and a description of what the query achieves
-	# it will automatically write it to the bottom of the table
-	cursor = conn.cursor()
-	quer = "ALTER TABLE Presets DROP COLUMN id;"
-	cursor.execute(quer)
-	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
-	cursor.execute(quer)
+    conn = database.get_db_connection()
+    conn.autocommit = True
 
-	extable = Table('Presets')
-	q = MySQLQuery.into(extable).columns("querval", "description").insert(queryin, descriptin)
-	print(q)
-	quer = str(q)
+    extable = Table('Presets')
+    q = MySQLQuery.into(extable).columns("querval", "description").insert(query_text, query_desc)
+    quer = str(q)
 
-	cursor.execute(quer)
+    cursor.execute(quer)
 
-	quer = "ALTER TABLE Presets DROP COLUMN id;"
-	cursor.execute(quer)
-	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
-	cursor.execute(quer)
+    conn.close()
 
-def edit_preset(conn, key, queryin, descriptin):
-	# to use this method you must pass in a connection,
-	# the id of a preset query,
-	# a preset query, and a description of what the query achieves
-	# it will update the query and description at the given id with new values.
-	# if queryin or descriptin = "NA" then it will not update the values written so
-	cursor = conn.cursor()
-	quer = "ALTER TABLE Presets DROP COLUMN id;"
-	cursor.execute(quer)
-	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
-	cursor.execute(quer)
+def edit_preset(query_id, query_text, description):
+    '''(int, str, str) -> None'''
+    # to use this method you must pass in a connection,
+    # the id of a preset query,
+    # a preset query, and a description of what the query achieves
+    # it will update the query and description at the given id with new values.
+    # if queryin or descriptin = "NA" then it will not update the values written so
 
-	if (queryin != "NA"):
-		quer = "UPDATE Presets SET querval='"+queryin+"' WHERE id="+str(key)+";"
-		cursor.execute(quer)
-	if (descriptin != "NA"):
-		quer = "UPDATE Presets SET description='"+descriptin+"' WHERE id="+str(key)+";"
-		cursor.execute(quer)
+    conn = database.get_db_connection()
+    conn.autocommit = False
 
-	quer = "ALTER TABLE Presets DROP COLUMN id;"
-	cursor.execute(quer)
-	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
-	cursor.execute(quer)
+    cursor = conn.cursor()
+    # quer = "ALTER TABLE Presets DROP COLUMN id"
+    # cursor.execute(quer)
+    # quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST"
+    # cursor.execute(quer)
+
+    quer = "UPDATE Presets SET querval=%s AND description=%s WHERE id=%s"
+    cursor.execute(quer, (query_text, description, query_id))
+
+    # quer = "ALTER TABLE Presets DROP COLUMN id"
+    # cursor.execute(quer)
+    # quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST"
+    # cursor.execute(quer)
+
+    conn.close()
 
 def remove_preset(conn, key):
-	# to use this method you must pass in a connection, and
-	# what number the preset's id is
-	cursor = conn.cursor()
-	quer = "ALTER TABLE Presets DROP COLUMN id;"
-	cursor.execute(quer)
-	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
-	cursor.execute(quer)
+    # to use this method you must pass in a connection, and
+    # what number the preset's id is
+    cursor = conn.cursor()
+    quer = "ALTER TABLE Presets DROP COLUMN id"
+    cursor.execute(quer)
+    quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST"
+    cursor.execute(quer)
 
-	quer = "DELETE FROM Presets WHERE id = " +key;
-	cursor.execute(quer)
+    quer = "DELETE FROM Presets WHERE id = %s"
+    cursor.execute(quer, (key,))
 
-	quer = "ALTER TABLE Presets DROP COLUMN id;"
-	cursor.execute(quer)
-	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
-	cursor.execute(quer)
-
-def get_preset(conn, key):
-	# to use this method you must pass in a connection
-	# and the id for the corresponding querval to return
-	# The querval returned is formatted to be ready to use
-	# as a query
-	cursor = conn.cursor()
-	quer = "ALTER TABLE Presets DROP COLUMN id;"
-	cursor.execute(quer)
-	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
-	cursor.execute(quer)
-
-	extable = Table('Presets')
-	q = MySQLQuery.from_(extable).select(
-		extable.querval
-	).where(
-		extable.id == key
-	)
-	print(q)
-	quer = str(q)
-
-	cursor.execute(quer)
-
-	row = cursor.fetchone()
-	strrow = str(row)
-
-	return (strrow[2:-3])
-
-def get_descriptin(conn, key):
-	# to use this method you must pass in a connection
-	# and the id for the corresponding description to return
-	# The description returned is formatted to be ready to displayed
-	cursor = conn.cursor()
-	quer = "ALTER TABLE Presets DROP COLUMN id;"
-	cursor.execute(quer)
-	quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST;"
-	cursor.execute(quer)
-
-	extable = Table('Presets')
-	q = MySQLQuery.from_(extable).select(
-		extable.description
-	).where(
-		extable.id == key
-	)
-	print(q)
-	quer = str(q)
-
-	cursor.execute(quer)
-
-	row = cursor.fetchone()
-	strrow = str(row)
-
-	return (strrow[2:-3])
+    quer = "ALTER TABLE Presets DROP COLUMN id"
+    cursor.execute(quer)
+    quer = "ALTER TABLE Presets ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST"
+    cursor.execute(quer)
