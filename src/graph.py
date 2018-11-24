792ffa1848d0559823b98820a3bddb1a6d02caa6
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-11-24 11:23:44 -0500

    remove try except in graph.py

--- src/graph.py
+++ src/graph.py
@@ -5,7 +5,6 @@ import query
 import database
 import textwrap
 
-
 def pie_chart(title, query):
     """Generate pie chart to display percentage of data
 
@@ -18,21 +17,17 @@ def pie_chart(title, query):
         [plot] -- the generated plot
     """
 
-    try:
-        # wrap the tile to display in appropriate length
-        title = textwrap.fill(title)
-        # set the first column as index
-        query = query.set_index(query.columns.values[0])
-        # generate pie chart, display legend, correct the percentage to 2 d.p.
-        graph = query.plot.pie(subplots=True, legend=False,
-                               autopct='%.2f', title=title)
-        # removed the y-axis label
-        plt.ylabel("")
-        # return the graph
-        return plt
-    except:
-        # return None if error
-        return None
+    # wrap the tile to display in appropriate length
+    title = textwrap.fill(title)
+    # set the first column as index
+    query = query.set_index(query.columns.values[0])
+    # generate pie chart, display legend, correct the percentage to 2 d.p.
+    graph = query.plot.pie(subplots=True, legend=False,
+                           autopct='%.2f', title=title)
+    # removed the y-axis label
+    plt.ylabel("")
+    # return the graph
+    return plt
 
 
 def line_chart(title, query):
@@ -47,20 +42,16 @@ def line_chart(title, query):
         [plot] -- the generated plot
     """
 
-    try:
-        # wrap the tile to display in appropriate length
-        title = textwrap.fill(title)
-        # set the first column as index
-        query = query.set_index(query.columns.values[0])
-        # plot and assign title to the graph
-        graph = query.plot.line(title=title)
-        # only allow integer in the graph
-        graph.locator_params(integer=True)
-        # return the graph
-        return plt
-    except:
-        # return None if error
-        return None
+    # wrap the tile to display in appropriate length
+    title = textwrap.fill(title)
+    # set the first column as index
+    query = query.set_index(query.columns.values[0])
+    # plot and assign title to the graph
+    graph = query.plot.line(title=title)
+    # only allow integer in the graph
+    graph.locator_params(integer=True)
+    # return the graph
+    return plt
 
 
 def bar_chart(title, query):
@@ -75,19 +66,14 @@ def bar_chart(title, query):
     Return:
         [plot] -- the generated plot
     """
-    try:
-        # wrap the tile to display in appropriate length
-        title = textwrap.fill(title)
-        # set the first column as index
-        query = query.set_index(query.columns.values[0])
-        # plot and assign title to the graph
-        graph = query.plot.bar(title=title, rot=0)
-        # return the graph
-        return plt
-    except:
-        # return None if error
-        return None
-
+    # wrap the tile to display in appropriate length
+    title = textwrap.fill(title)
+    # set the first column as index
+    query = query.set_index(query.columns.values[0])
+    # plot and assign title to the graph
+    graph = query.plot.bar(title=title, rot=0)
+    # return the graph
+    return plt
 
 if __name__ == "__main__":
     connection = database.get_db_connection()
